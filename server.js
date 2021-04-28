3f778e41de2dd484feab5561200c819c6516a691
emicon-web <emiconweb@emicon.co.in>
2021-04-28 15:06:15 +0530

    df

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ require('./startup/prod') (app);
 require('./models/db');
 
 require('./startup/routes') (app);
-app.use(express.static(path.join(__dirname, 'client')))
+app.use(express.static(path.join(__dirname, '/client')))
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'))
+    res.sendFile(path.join(__dirname, '/client', 'index.html'))
 })
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
