5814b9e4688bf816fa0954b65a1fbf31935e0322
emicon-web <emiconweb@emicon.co.in>
2021-04-28 16:41:20 +0530

    Fd

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const csp = require('helmet-csp')
 // require('./config/config');
 require('./startup/prod') (app);
 require('./models/db');
-
+app.use(requireHTTPS);
 require('./startup/routes') (app);
 app.use(express.static(path.join(__dirname, '/client')))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client', 'index.html'))
 })
-
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 var PORT = process.env.PORT || 5000;
 var serverhost = process.env.YOUR_HOST || '0.0.0.0';
 
