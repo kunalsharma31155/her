f726af8a40020221df1a50c722962237ea9eaa3f
emicon-web <emiconweb@emicon.co.in>
2021-04-28 15:28:28 +0530

    df

--- server.js
+++ server.js
@@ -12,14 +12,7 @@ app.use(express.static(path.join(__dirname, '/client')))
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client', 'index.html'))
 })
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+
   app.use(csp({
     directives: {
       defaultSrc: ["'self'"],
--- startup/routes.js
+++ startup/routes.js
@@ -3,13 +3,14 @@ const bodyParser = require("body-parser");
 const express = require('express');
 const cors = require('cors');
 var path = require( "path" );
-
+const helmet = require('helmet');
 const error = require('../middleware/error');
 const user = require('../api/user.router');
 
 
 
 module.exports = function(app){
+    app.use(helmet());
     app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
     app.use(cors());
