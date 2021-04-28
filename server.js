f72a1d54f19bcd17cfbbe7a258e231504104930e
emicon-web <emiconweb@emicon.co.in>
2021-04-28 14:53:53 +0530

    df

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-require('./config/config');
+// require('./config/config');
 require('./startup/prod') (app);
 require('./models/db');
 
@@ -26,3 +26,9 @@ app.listen( PORT,err=>{
     console.log(`Server Started On Portt : ${process.env.PORT}`);
 }) 
 const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://engagement:Mongodb@123@cluster0.r17es.mongodb.net/bet?retryWrites=true&w=majority" , { useNewUrlParser: true , useUnifiedTopology: true ,useCreateIndex: true } ,
+    (err) => {
+    if(!err) { console.log('Mongo DB connected succeeded'); }
+    else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined , 2)); }
+});
